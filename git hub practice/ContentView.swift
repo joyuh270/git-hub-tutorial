9d4fcf852f61084192ae70d37b152920e16365fd
me <joyaalhadeff125@gmail.com>
2024-07-12 12:56:10 -0600

    Initial Commit

---
+++ git hub practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  git hub practice
+//
+//  Created by Joya Alhadeff on 7/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ git hub practice/git_hub_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_hub_practiceApp.swift
+//  git hub practice
+//
+//  Created by Joya Alhadeff on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct git_hub_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
